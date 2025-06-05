c7cfaa7966fc0e795fb4744db86be779b188289c
Nicolas-Puchois <nicolas.puchois@laposte.net>
2025-06-05 11:11:41 +0200

    add helmet config

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import helmet from "helmet";
 import path from "path";
 import dotenv from "dotenv";
 import indexRoutes from "./routes/index.js";
+const API_URL = process.env.API_URL;
 dotenv.config();
 // création de l'application
 const app = express();
@@ -19,3 +20,27 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("serveur lancé :\nhttp://localhost:" + PORT);
 });
+// helmet
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: [
+          "'self'",
+          "'unsafe-inline'",
+          "'unsafe-eval'",
+          "localhost:*",
+        ],
+        connectSrc: ["'self'", "ws://localhost:*", "http://localhost:8000"],
+        imgSrc: ["'self'", "data:", "blob:", "http://localhost:8000"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        formAction: ["'self'"],
+        baseUri: ["'self'"],
+      },
+    },
+    crossOriginEmbedderPolicy: false,
+    crossOriginResourcePolicy: { policy: "cross-origin" },
+    crossOriginOpenerPolicy: false,
+  })
+);
